223e92e7dc480d9da57748a434d81e52e4ab2df5
benjaminangafua <benjaminarkutl2017@gmail.com>
2023-11-04 16:46:04 +0000

    Setting nav

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const body = document.querySelector("body")
-body.innerHTML = `
-<h1>Place to find a comfortable home and with ease of connection</h1>
-<p>Life is better with comfort</p>`
+const querySelector = function(selector) {
+    return document.querySelector(selector)
+}
+
+
+// querySelector("body").innerHTML = `
+// <h1>Place to find a comfortable home and with ease of connection</h1>
+// <p>Life is better with comfort</p>`
